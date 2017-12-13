b922af1d5bace25d9bc64012135d1771ded9644c
avoup <avoup.g@gmail.com>
2017-12-13 21:04:19 +0400

    kind of complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,49 @@
 $(document).ready(function(){
-  (function() {
-    $.scrollify({
-  		section : "section",
-  		sectionName : "section-name",
-  		easing: "easeOutExpo",
-  		scrollSpeed: 1000,
-  		offset : 0,
-  		scrollbars: false,
-  		before:function() {},
-  		after:function() {}
-  	});
 
-    $('li').click(function() {
-    $.scrollify.move('#' + $(this).attr('id'))
-  })
+    (function() {
+      $.scrollify({
+    		section : "section",
+    		sectionName : "section-name",
+    		easing: "easeOutExpo",
+    		scrollSpeed: 500,
+    		offset : 0,
+    		scrollbars: false,
+        standardScrollElements: "img",
+    		before:function() {},
+    		after:function() {}
+    	});
 
+      $('li').click(function() {
+      $.scrollify.move('#_' + $(this).attr('id'))
+    })
   })()
+
+
+    $("#icon").click(function(){
+      $(this).toggleClass("clicked");
+      $('.side_panel').toggleClass("open");
+    });
+
+    $(".site").click(function(e){
+      switch(e.target.id){
+        case "1":
+          $("#dsk").attr("src","assets/img/YOUTHforWHO.jpg");
+          $("#lap").attr("src","assets/img/YOUTHforWHO.jpg");
+          $("#tab").attr("src","assets/img/YOUTHforWHO.jpg");
+          $("#mob").attr("src","assets/img/YOUTHforWHO_mobile.jpg");
+          break;
+        case "2":
+          $("#dsk").attr("src","assets/img/YFW_green.jpg");
+          $("#lap").attr("src","assets/img/YFW_green.jpg");
+          $("#tab").attr("src","assets/img/YFW_green.jpg");
+          $("#mob").attr("src","assets/img/YFW_green.jpg");
+          break;
+        case "3":
+          $("#dsk").attr("src","assets/img/site.jpg");
+          $("#lap").attr("src","assets/img/site.jpg");
+          $("#tab").attr("src","assets/img/site.jpg");
+          $("#mob").attr("src","assets/img/site.jpg");
+      }
+    })
+
 })
